47758cf04cab23acc30725a4c33f7308260278cd
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2022-06-13 01:34:45 +0000

    link

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="">kizamu!</a>
+          Welcome to <a href="https://github.com/Shirtiny/kizamu">kizamu!</a>
         </h1>
 
         <p className={styles.description}>
