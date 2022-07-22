7a2baaf9660c899a0d3b56f7232b1a962074e704
shirtiny <shirtiny@gmail.com>
2022-07-22 07:17:01 +0000

    Updates

---
+++ _next/static/chunks/696.7c6ae30bb235806f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[696],{3344:function(t,s,n){"use strict";n.r(s);var e=n(3715),u=n(5675),c=n.n(u),a=n(5893),h=(0,e.Z)("div")({name:"StyledAppLayout",class:"sgoq527"});s.default=function(){return(0,a.jsx)(h,{children:(0,a.jsx)(c(),{src:"https://picsum.photos/300",width:300,height:300,alt:"random"})})},n(1071)},1071:function(){}}]);
