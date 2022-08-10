f961097479be66e8e2e3e17a4ba5426257fba963
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2022-08-10 03:44:37 +0000

    rem

--- layouts/app.tsx
+++ layouts/app.tsx
@@ -2,18 +2,41 @@ import { FC } from "react";
 import { styled } from "@linaria/react";
 
 const StyledAppLayout = styled.div`
+  :global() {
+    html {
+      font-size: 16px;
+    }
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+  }
+  
   height: 100vh;
-  padding: 32px 24px;
+  padding: 2rem 1.5rem;
   background-color: #f6f6f6;
   .container {
     height: 100%;
     display: flex;
     background-color: #fdfdfd;
 
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0rem 1.5625rem 3.125rem -0.75rem;
     overflow: hidden;
     .aside {
-      border: 1px solid #e8e8e8;
+      width: 6.25rem;
+      border: 0.0625rem solid #e8e8e8;
     }
     .main {
       flex: 1;
@@ -30,8 +53,8 @@ const AppLayout: FC<IProps> = () => {
       <div className="container">
         <aside className="aside">
           <nav className="nav">
-            <a>latest</a>
-            <a>gallery</a>
+            {/* <a>latest</a>
+            <a>gallery</a> */}
           </nav>
         </aside>
         <main className="main"></main>
