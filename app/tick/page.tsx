462579ecb11589ca2cacc0bb50ccf87d90e1de01
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2023-01-06 11:43:56 +0000

    page type

--- app/tick/page.tsx
+++ app/tick/page.tsx
@@ -1,19 +1,48 @@
-'use client';
-// import { FC, memo } from "react";
+"use client";
 import { HashRouter } from "react-router-dom";
-import { NextPage } from "next";
-import TickAppLayout from "../../layouts/TickApp";
+import styled from "styled-components";
 import TickAside from "../../components/tick/Aside";
 import AppRoutes from "../../router/AppRoutes";
 import routerConfig from "../../router/config";
 
-const TickPage: NextPage = () => {
+const StyledTickAppLayout = styled.div`
+  height: 100vh;
+  padding: 2rem 1.5rem;
+  background-color: var(--color-primary-background-color-dark);
+  transition: background-color 0.4s ease-in-out;
+
+  .container {
+    height: 100%;
+    display: flex;
+    background-color: var(--color-primary-background-color);
+    transition: background-color 0.4s ease-in-out;
+    box-shadow: rgba(0, 0, 0, 0.25) 0rem 1.5625rem 3.125rem -0.75rem;
+    overflow: hidden;
+
+    .aside {
+      width: 6.25rem;
+    }
+
+    .main {
+      flex: 1;
+      overflow: hidden;
+    }
+  }
+`;
+
+const TickPage = () => {
   return (
     <HashRouter>
-      <TickAppLayout
-        Aside={<TickAside />}
-        Main={<AppRoutes routes={routerConfig.tick} />}
-      />
+      <StyledTickAppLayout>
+        <div className="container">
+          <aside className="aside">
+            <TickAside />
+          </aside>
+          <div className="main">
+            <AppRoutes routes={routerConfig.tick} />
+          </div>
+        </div>
+      </StyledTickAppLayout>
     </HashRouter>
   );
 };
